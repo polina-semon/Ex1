00d4491f6704ff3604d7ad636544af47786689b1
polina-semon <semunda@yandex.ru>
2020-11-23 19:33:53 +0300

    вроде, оно работает

---
+++ ex2_lab10.py
@@ -0,0 +1,34 @@
+# Пыталась считать время, но по времени ничего практически не меняется
+# иногда код работает быстрее, иногда медленнее
+
+import os
+import re
+import time
+import threading
+
+
+def thread_job(suffix):
+    ip = "192.168.178." + str(suffix)
+    ping_out = os.popen("ping -q -c2 " + ip, "r")  # получение вердикта
+    print("... pinging ", ip)
+    while True:
+        line = ping_out.readline()
+        if not line:
+            break
+        n_received = received_packages.findall(line)
+        if n_received:
+            print(ip + ": " + status[int(n_received[0])])
+
+
+start = time.time()
+received_packages = re.compile(r"(\d) received")
+status = ("no response", "alive but losses", "alive")
+
+threads = [threading.Thread(target=thread_job(suffix)) for suffix in range(20, 30)]
+for thread in threads:
+    thread.start()
+for thread in threads:
+    thread.join()
+end = time.time()
+t = end - start
+print(t)
