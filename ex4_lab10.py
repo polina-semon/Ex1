fdbeee34b435139490c5c1612bc9231d08ba71fd
polina-semon <semunda@yandex.ru>
2020-11-23 20:11:39 +0300

    вроде, оно работает

---
+++ ex4_lab10.py
@@ -0,0 +1,28 @@
+# t = 2,45 - 3,46 без потоков
+# t = 3,03 3,29 2,51
+
+import urllib.request
+import time
+import threading
+
+
+def thread_job(url):
+    with urllib.request.urlopen(url) as u:
+        return u.read()
+
+
+if __name__ == '__main__':
+    start = time.time()
+    urls = [
+        'https://www.yandex.ru', 'https://www.google.com',
+        'https://habrahabr.ru', 'https://www.python.org',
+        'https://isocpp.org',
+    ]
+    threads = [threading.Thread(target=thread_job(url)) for url in urls]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+    end = time.time()
+    t = end - start
+    print(t)
